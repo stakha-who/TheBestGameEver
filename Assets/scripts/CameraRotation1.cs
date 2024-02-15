f2aa1f46a8d8198e41cc8def07dfebe7f46306ce
stakha-who <vita-astakhova@mail.ru>
2024-02-15 23:09:06 +0300

    Вращение камеры

---
+++ Assets/scripts/CameraRotation1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    public Transform CameraAxisTransform;
+    public float MinAngle = -20;
+    public float MaxAngle = 30;
+    public float RotationSpeed = 1;
+
+    void Update()
+    {
+        float newAngleY = transform.localEulerAngles.y + RotationSpeed * Input.GetAxis("Mouse X");
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+
+        float newAngleX = CameraAxisTransform.localEulerAngles.x - RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX > 180)
+        {
+            newAngleX -= 360;
+        }
+        newAngleX = Mathf.Clamp(newAngleX, MinAngle, MaxAngle);
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public float Gravity = 9.8f;
+    public float JumpForce = 7;
+    public float Speed = 5;
+
+    private Vector3 _moveVector;
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+  
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        if (_moveVector != Vector3.zero)
+        {
+            animator.SetBool("isRun", true);
+        }
+        else
+        {
+            animator.SetBool("isRun", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -JumpForce;
+            animator.SetBool("isGrounded", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _characterController.Move(_moveVector * Speed * Time.fixedDeltaTime);
+
+        _fallVelocity += Gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+            animator.SetBool("isGrounded", true);
+        }
+    }
+}
