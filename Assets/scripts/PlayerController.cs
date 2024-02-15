327cc648f21a7f7a78c7aaaf8a6ec8a389fb805e
stakha-who <vita-astakhova@mail.ru>
2024-02-16 00:56:00 +0300

    законченный вариант движения персонажа и вращения мышки

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Gravity = 9.8f;
+    public float JumpForce;
+    public float Speed;
+
+    private Vector3 _moveVector;
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+  
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -JumpForce;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _characterController.Move(_moveVector * Speed * Time.fixedDeltaTime);
+
+        _fallVelocity += Gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+}
